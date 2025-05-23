96453693805185401b3459afff7e94bb4c6c0c0c
Vutha Sothea <vuthasothea@gmail.com>
2025-05-23 19:20:25 +0700

    using usePathname and useSearchParams hook

--- src/app/dashboard/overview/[account]/page.tsx
+++ src/app/dashboard/overview/[account]/page.tsx
@@ -1,12 +1,18 @@
 "use client"
 
-import { useParams } from "next/navigation"
+import { useParams, usePathname, useSearchParams } from "next/navigation"
 
 export default function Page() {
 
   const param = useParams<{account: string}>();
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
 
   return (
-    <h1>Over View: {param.account}</h1>
+    <>
+      <h1>Over View: {param.account}</h1>
+      <h2>Pathname: {pathname}</h2>
+      <h2>Search Params: {searchParams.get("id")}</h2>
+    </>
   )
 }
